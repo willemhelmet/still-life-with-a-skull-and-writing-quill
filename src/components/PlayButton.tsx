198f4100644c2066ba6efa9d960bd66247c7400d
Willem Helmet Pickleman <willemhelmet@gmail.com>
2025-12-17 14:50:15 -0500

    removed controls

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -26,23 +26,23 @@ export const PlayButton = () => {
         >
           {buttonText}
         </button>
-        <div className="text-center max-w-md px-4">
-          <h2 className="text-lg font-semibold mb-2">Controls</h2>
-          {isMobile ? (
-            <ul className="text-sm space-y-1 text-gray-200">
-              <li>Use the Joystick to Move & Look</li>
-            </ul>
-          ) : (
-            <ul className="text-sm space-y-1 text-gray-200">
-              <li>
-                <span className="font-bold">W, A, S, D</span> to Move
-              </li>
-              <li>
-                <span className="font-bold">Esc</span> to Pause
-              </li>
-            </ul>
-          )}
-        </div>
+        {/* <div className="text-center max-w-md px-4"> */}
+        {/*   <h2 className="text-lg font-semibold mb-2">Controls</h2> */}
+        {/*   {isMobile ? ( */}
+        {/*     <ul className="text-sm space-y-1 text-gray-200"> */}
+        {/*       <li>Use the Joystick to Move & Look</li> */}
+        {/*     </ul> */}
+        {/*   ) : ( */}
+        {/*     <ul className="text-sm space-y-1 text-gray-200"> */}
+        {/*       <li> */}
+        {/*         <span className="font-bold">W, A, S, D</span> to Move */}
+        {/*       </li> */}
+        {/*       <li> */}
+        {/*         <span className="font-bold">Esc</span> to Pause */}
+        {/*       </li> */}
+        {/*     </ul> */}
+        {/*   )} */}
+        {/* </div> */}
       </div>
     </>
   );
